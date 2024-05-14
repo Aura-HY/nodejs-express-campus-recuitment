2bdbfea9bf956128b06b48fb44665b66145098d2
csilenti <147641029+csilenti@users.noreply.github.com>
2024-05-14 12:17:44 +0800

    重新撰写景清后端数据

--- src/service/userService.js
+++ src/service/userService.js
@@ -114,14 +114,49 @@ exports.deleteMessage = async (messageId) => {
     const deleteMessage = await userDao.deleteMessage(messageId);
     return deleteMessage;
 };
-//景清
-//读取用户的简历
+// 景清
+// 读取用户的简历
 exports.getUserResumes = async (userId) => {
     const userResumes = await userDao.getUserResumes(userId);
     return userResumes;
 };
-//读取用户简历的详细信息
+// 读取用户简历的详细信息
 exports.getUserResumesInfo = async (resumeId) => {
     const userResumesInfo = await userDao.getUserResumesInfo(resumeId);
     return userResumesInfo;
+};
+// 读取用户的昵称和头像
+exports.getUserBasic = async (userId) => {
+    const getUserBasic = await userDao.getUserBasic(userId);
+    return getUserBasic;
+};
+// 统计用户收藏的简历份数总数
+exports.favoritesCollectionsSum = async (userId) => {
+    const userResumesInfo = await userDao.favoritesCollectionsSum(userId);
+    return Number(userResumesInfo[0].sum);
+};
+// 统计用户沟通过的聊天总数
+exports.sessionSum = async (userId) => {
+    const sessionSum = await userDao.sessionSum(userId);
+    return Number(sessionSum[0].sessionSum);
+};
+// 统计用户已投简历总数
+exports.resumeTrueSum = async (userId) => {
+    const resumeTrueSum = await userDao.resumeTrueSum(userId);
+    return Number(resumeTrueSum[0].resumeTrueSum);
+};
+// 统计用户待面试总数
+exports.interviewedSum = async (userId) => {
+    const interviewedSum = await userDao.interviewedSum(userId);
+    return Number(interviewedSum[0].interviewedSum);
+};
+// 添加用户新简历
+exports.addNewResumeInfo = async (resumeName, name, gender, grade, academy, classNumber, studentId, wechat, phone, healthCertificate, curriculumVitae, workExperience, honorCertificate, userId) => {
+    const addNewResumeInfo = await userDao.addNewResumeInfo(resumeName, name, gender, grade, academy, classNumber, studentId, wechat, phone, healthCertificate, curriculumVitae, workExperience, honorCertificate, userId);
+    return addNewResumeInfo;
+};
+//读取用户收藏的职位
+exports.getUserFavoritesJob = async (userId) => {
+    const getUserFavoritesJob = await userDao.getUserFavoritesJob(userId);
+    return getUserFavoritesJob;
 };
